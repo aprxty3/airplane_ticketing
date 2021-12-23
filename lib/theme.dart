e4a6b97ba11186d00635b4c4c26d3026c2592139
aprxrt3 <ajiprxty3@gmail.com>
2021-12-24 02:48:41 +0700

    add Color theme

---
+++ lib/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//COLORS
+Color kPrimaryColor = const Color(0xff5C40CC); //Untuk Container Keseringan
+Color sbPrimaryColor = const Color(0xff3F32B2); //Untuk Gradasi
+Color sbbPrimaryColor = const Color(0xffE970A2); //untuk Gradasi dan Font juga
+Color sGreenColor = const Color(0xff0EC3AE); //untuk Font
+Color sPrimaryColor = const Color(0xff1F1449); //Untuk Font Keseringan
+Color sWhiteColor = Colors.white; //Untuk Font Juga
+Color sGreyColor = const Color(0xff9698A9); //Untuk Font Keseringan
+Color s2GreyColor = const Color(0xffDBD7EC); //Untuk Container TextFormField
+Color kBgColor = const Color(0xffFAFAFA); //Buat Background
