e9319f9ef76c62e33f2448ac1405bf338e49397c
aprxrt3 <ajiprxty3@gmail.com>
2021-12-24 03:27:28 +0700

    add TextStyle

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 //COLORS
 Color kPrimaryColor = const Color(0xff5C40CC); //Untuk Container Keseringan
@@ -10,3 +11,228 @@ Color sWhiteColor = Colors.white; //Untuk Font Juga
 Color sGreyColor = const Color(0xff9698A9); //Untuk Font Keseringan
 Color s2GreyColor = const Color(0xffDBD7EC); //Untuk Container TextFormField
 Color kBgColor = const Color(0xffFAFAFA); //Buat Background
+
+//TextStyle
+TextStyle mainStyle = GoogleFonts.poppins(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+  color: sPrimaryColor,
+);
+
+TextStyle shadowStyle = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w300,
+  color: sGreyColor,
+);
+
+TextStyle ratingStyle1 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: sPrimaryColor,
+);
+
+TextStyle ratingStyle2 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: sWhiteColor,
+);
+
+//Button
+TextStyle buttonStyle = GoogleFonts.poppins(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  color: sWhiteColor,
+);
+
+//Onboarding
+TextStyle onboardStyle1 = GoogleFonts.poppins(
+  fontSize: 32,
+  fontWeight: FontWeight.w600,
+  color: sWhiteColor,
+);
+
+TextStyle onboardStyle2 = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w300,
+  color: sWhiteColor,
+);
+
+//SIGN
+TextStyle signStyle = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  color: sPrimaryColor,
+);
+
+TextStyle textformStyle = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  color: sPrimaryColor,
+);
+
+//HOME
+TextStyle destinationStyle1 = GoogleFonts.poppins(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  color: sPrimaryColor,
+);
+
+TextStyle destinationStyle2 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w300,
+  color: sGreyColor,
+);
+
+TextStyle typeStyle = GoogleFonts.poppins(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  color: sPrimaryColor,
+);
+
+//DETAILS
+TextStyle namedesStyle1 = GoogleFonts.poppins(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+  color: sWhiteColor,
+);
+
+TextStyle namedesStyle2 = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w300,
+  color: sWhiteColor,
+);
+
+TextStyle detailStyle1 = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  color: sPrimaryColor,
+);
+
+TextStyle detailStyle2 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  color: sPrimaryColor,
+);
+
+TextStyle detailPriceStyle1 = GoogleFonts.poppins(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  color: sPrimaryColor,
+);
+
+TextStyle detailPriceStyle2 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w300,
+  color: sGreyColor,
+);
+
+//SEAT
+TextStyle availableStyle = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  color: sPrimaryColor,
+);
+
+TextStyle seatStyle1 = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  color: sGreyColor,
+);
+
+TextStyle seatStyle2 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  color: sWhiteColor,
+);
+
+TextStyle seatStyle3 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w300,
+  color: sGreyColor,
+);
+
+TextStyle seatStyle4 = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  color: sPrimaryColor,
+);
+
+TextStyle seatPriceStyle = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  color: kPrimaryColor,
+);
+
+//CHECKOUT
+TextStyle coStyle1 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  color: sPrimaryColor,
+);
+
+TextStyle coStyle2 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  color: sbbPrimaryColor,
+);
+
+TextStyle coStyle3 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  color: sGreenColor,
+);
+
+TextStyle coStyle4 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  color: kPrimaryColor,
+);
+
+TextStyle paymentStyle1 = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  color: sPrimaryColor,
+);
+
+TextStyle paymentStyle2 = GoogleFonts.poppins(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  color: sPrimaryColor,
+);
+
+TextStyle paymentStyle3 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w300,
+  color: sGreyColor,
+);
+
+//BONUS & SUCCESS
+TextStyle bonustStyle1 = GoogleFonts.poppins(
+  fontSize: 32,
+  fontWeight: FontWeight.w600,
+  color: sPrimaryColor,
+);
+
+TextStyle bonusStyle2 = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w300,
+  color: sGreyColor,
+);
+
+TextStyle bonusNameStyle1 = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w300,
+  color: sWhiteColor,
+);
+
+TextStyle bonusNameStyle2 = GoogleFonts.poppins(
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+  color: sWhiteColor,
+);
+
+TextStyle bonusNameStyle3 = GoogleFonts.poppins(
+  fontSize: 26,
+  fontWeight: FontWeight.w500,
+  color: sWhiteColor,
+);
