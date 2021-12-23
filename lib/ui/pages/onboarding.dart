9a668b14cd70d230ed6f2905ab4afacf917bbdca
aprxrt3 <ajiprxty3@gmail.com>
2021-12-24 04:17:31 +0700

    create OnboardingPage

---
+++ lib/ui/pages/onboarding.dart
@@ -0,0 +1,60 @@
+import 'package:airplane_ticketing/theme.dart';
+import 'package:flutter/material.dart';
+
+class OnboardPage extends StatelessWidget {
+  const OnboardPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/image_get_started.png',
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Fly Like a Bird',
+                  style: onboardStyle1,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Explore new world with us and let \nyourself get an amazing experiences',
+                  style: onboardStyle2,
+                  textAlign: TextAlign.center,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 50, bottom: 80),
+                  width: 220,
+                  height: 55,
+                  // color: Colors.blue,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: kPrimaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(17),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Get Started',
+                      style: buttonStyle,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
