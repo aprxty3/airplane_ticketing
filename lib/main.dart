bcd1bca92246c468f030846c8b54951e9c96d2c5
aprxrt3 <ajiprxty3@gmail.com>
2021-12-24 03:53:17 +0700

    navigate to SplashPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
+import 'package:airplane_ticketing/ui/pages/splash.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp( const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ), 
-      ),
+    
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+      home: SplashPage(),
     );
   }
 }
