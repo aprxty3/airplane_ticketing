5aaf82ab92d18f4553ba5a66feedffc55b641837
aprxrt3 <ajiprxty3@gmail.com>
2021-12-24 14:31:54 +0700

    add input form

--- lib/ui/pages/sign_up.dart
+++ lib/ui/pages/sign_up.dart
@@ -1,15 +1,175 @@
+import 'package:airplane_ticketing/theme.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatelessWidget {
-  const SignUpPage({ Key? key }) : super(key: key);
+  const SignUpPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Widget title() {
+      return Container(
+        margin: const EdgeInsets.only(top: 10),
+        child: Text(
+          'Join us and get \nyour next journey',
+          style: mainStyle,
+        ),
+      );
+    }
+
+    Widget textform() {
+      Widget name() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Full Name',
+                style: signStyle,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: sPrimaryColor,
+                decoration: InputDecoration(
+                  hintText: 'Your Full Name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: kPrimaryColor),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      }
+
+      Widget email() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Full Name',
+                style: signStyle,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: sPrimaryColor,
+                decoration: InputDecoration(
+                  hintText: 'Your Full Name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: kPrimaryColor),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      }
+
+      Widget password() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Full Name',
+                style: signStyle,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: sPrimaryColor,
+                decoration: InputDecoration(
+                  hintText: 'Your Full Name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: kPrimaryColor),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      }
+
+      Widget hobby() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Full Name',
+                style: signStyle,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: sPrimaryColor,
+                decoration: InputDecoration(
+                  hintText: 'Your Full Name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: kPrimaryColor),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      }
+
+      return Container(
+        margin: const EdgeInsets.only(top: 30, bottom: 50),
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        decoration: BoxDecoration(
+          color: sWhiteColor,
+          borderRadius: BorderRadius.circular(defaultRadius),
+        ),
+        child: Column(
+          children: [
+            name(),
+            email(),
+            password(),
+            hobby(),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
-      body: Column(children: [
-        Text('hahaha')
-      ],),
-      
+      backgroundColor: kBgColor,
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+          children: [
+            title(),
+            textform(),
+          ],
+        ),
+      ),
     );
   }
 }
