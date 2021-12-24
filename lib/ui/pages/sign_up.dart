30c50a2c071c103c2aadcb94b5eb8be58876c07e
aprxrt3 <ajiprxty3@gmail.com>
2021-12-24 13:41:29 +0700

    build sign up menu

---
+++ lib/ui/pages/sign_up.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        Text('hahaha')
+      ],),
+      
+    );
+  }
+}
