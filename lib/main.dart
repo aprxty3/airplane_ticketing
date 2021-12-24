2b67f9d569121985b2d9cedf651621b695fbc1b2
aprxrt3 <ajiprxty3@gmail.com>
2021-12-24 13:30:41 +0700

    implement navigator route

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,19 @@ import 'package:airplane_ticketing/ui/pages/onboarding.dart';
 import 'package:airplane_ticketing/ui/pages/splash.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp( const MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
-      home: OnboardPage(),
+      routes: {
+        '/': (context) => const SplashPage(),
+        '/onboard': (context) => const OnboardPage(),
+      },
     );
   }
 }
