9e2e794e1b2f4fe5af179dd01d38323efd25f3ce
aprxrt3 <ajiprxty3@gmail.com>
2021-12-24 13:31:13 +0700

    implement Timer and Navigator route

--- lib/ui/pages/splash.dart
+++ lib/ui/pages/splash.dart
@@ -1,9 +1,29 @@
+import 'dart:async';
+
+import 'package:airplane_ticketing/ui/pages/onboarding.dart';
 import 'package:flutter/material.dart';
 import 'package:airplane_ticketing/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    Timer(
+      const Duration(seconds: 5),
+      () {
+        Navigator.pushNamed(context, '/onboard');
+      },
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
