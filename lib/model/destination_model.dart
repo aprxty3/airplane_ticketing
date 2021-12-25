7c83e7930b1ab0de68cdfb3416cb5291a742a990
aprxrt3 <ajiprxty3@gmail.com>
2021-12-25 15:55:02 +0700

    destination model

---
+++ lib/model/destination_model.dart
@@ -0,0 +1,13 @@
+class destinationModel {
+  final String imageUrl;
+  final double rate;
+  final String destination1;
+  final String destination2;
+
+  destinationModel({
+    this.imageUrl = '',
+    this.rate = 0.0,
+    this.destination1 = '',
+    this.destination2 = '',
+  });
+}
