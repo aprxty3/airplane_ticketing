edb72604550968447703716a32ec32565e0d165e
aprxrt3 <ajiprxty3@gmail.com>
2021-12-25 16:56:42 +0700

    add inkwell

--- lib/ui/widget/destination.dart
+++ lib/ui/widget/destination.dart
@@ -7,7 +7,7 @@ class PopularDestination extends StatelessWidget {
   final double rate;
   final String destination1;
   final String destination2;
-  
+
   const PopularDestination({
     Key? key,
     required this.imageUrl,
@@ -18,75 +18,79 @@ class PopularDestination extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: 323,
-      margin: EdgeInsets.symmetric(horizontal: 10),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(defaultRadius),
-        color: sWhiteColor,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: 180,
-            height: 220,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(defaultRadius),
-              image: DecorationImage(
-                image: AssetImage(imageUrl),
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        width: 200,
+        height: 323,
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(defaultRadius),
+          color: sWhiteColor,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 180,
+              height: 220,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(defaultRadius),
+                image: DecorationImage(
+                  image: AssetImage(imageUrl),
+                ),
               ),
-            ),
-            child: Align(
-              alignment: Alignment.topRight,
-              child: Container(
-                padding: const EdgeInsets.only(left: 5.5, bottom: 6, right: 3),
-                width: 55,
-                height: 30,
-                decoration: BoxDecoration(
-                  color: sWhiteColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(defaultRadius),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  padding:
+                      const EdgeInsets.only(left: 5.5, bottom: 6, right: 3),
+                  width: 55,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    color: sWhiteColor,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(defaultRadius),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset(
+                        'assets/icon_star.png',
+                        width: 24,
+                      ),
+                      Text(
+                        '$rate',
+                        style: ratingStyle1,
+                      )
+                    ],
                   ),
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.asset(
-                      'assets/icon_star.png',
-                      width: 24,
-                    ),
-                    Text(
-                      '$rate',
-                      style: ratingStyle1,
-                    )
-                  ],
-                ),
               ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 10, top: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  destination1,
-                  style: destinationStyle1,
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  destination2,
-                  style: destinationStyle2,
-                ),
-              ],
+            Container(
+              margin: const EdgeInsets.only(left: 10, top: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    destination1,
+                    style: destinationStyle1,
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    destination2,
+                    style: destinationStyle2,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
