924f55a707819aa0b17d584507703a8c2a39896d
aprxrt3 <ajiprxty3@gmail.com>
2021-12-25 09:51:56 +0700

    create header

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:airplane_ticketing/main.dart';
 import 'package:airplane_ticketing/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +7,48 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget mainPage() {
+      Widget header() {
+        return Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Apa Kabs! \nAji Prasetyo',
+                  style: mainStyle,
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  'Where to fly today?',
+                  style: shadowStyle,
+                )
+              ],
+            ),
+            
+          ],
+        );
+      }
+
+      return Padding(
+        padding: const EdgeInsets.only(left: 24, top: 30),
+        child: Column(
+          children: [
+            header(),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kBgColor,
+      body: ListView(
+        children: [
+          mainPage(),
+        ],
+      ),
     );
   }
 }
