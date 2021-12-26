7c26942d8aad1fea750b083503e999b68cc73196
aprxrt3 <ajiprxty3@gmail.com>
2021-12-27 05:21:13 +0700

    fixing design slicing

--- lib/ui/widget/interest.dart
+++ lib/ui/widget/interest.dart
@@ -4,23 +4,20 @@ import '../../theme.dart';
 
 class Interest extends StatelessWidget {
   final String interest1;
-  final String interest2;
-
+  
   const Interest({
     Key? key,
     required this.interest1,
-    required this.interest2,
+    
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Container(
-          margin: const EdgeInsets.only(right: 23),
-          width: 122,
-          child: Row(
+    return Expanded(
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
             children: [
               Image.asset(
                 'assets/icon_check.png',
@@ -35,23 +32,9 @@ class Interest extends StatelessWidget {
               ),
             ],
           ),
-        ),
-        Row(
-          children: [
-            Image.asset(
-              'assets/icon_check.png',
-              width: 16,
-            ),
-            const SizedBox(
-              width: 6,
-            ),
-            Text(
-              interest2,
-              style: detailStyle2,
-            ),
-          ],
-        )
-      ],
+         
+        ],
+      ),
     );
   }
 }
