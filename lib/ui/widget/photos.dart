931f41571e29d1ca86baba7495ae2791587deab7
aprxrt3 <ajiprxty3@gmail.com>
2021-12-27 03:11:19 +0700

    created widget photos

---
+++ lib/ui/widget/photos.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../theme.dart';
+
+class Photos extends StatelessWidget {
+  final String imageUrl;
+
+  const Photos({
+    Key? key,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      margin: const EdgeInsets.only(right: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(defaultRadius),
+        image: DecorationImage(
+          image: AssetImage(imageUrl),
+        ),
+      ),
+    );
+  }
+}
