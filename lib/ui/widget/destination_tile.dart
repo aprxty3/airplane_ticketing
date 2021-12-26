32b0efc5019a207e1ea2d2f0e979ad77d571c2cb
aprxrt3 <ajiprxty3@gmail.com>
2021-12-27 05:20:51 +0700

    add detail route

--- lib/ui/widget/destination_tile.dart
+++ lib/ui/widget/destination_tile.dart
@@ -18,62 +18,67 @@ class DesTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(defaultRadius),
-        color: sWhiteColor,
-      ),
-      padding: const EdgeInsets.all(10),
-      child: Row(
-        children: [
-          Container(
-            width: 70,
-            height: 70,
-            margin: const EdgeInsets.only(right: 16),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(imageUrl),
-                fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, '/detail');
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(defaultRadius),
+          color: sWhiteColor,
+        ),
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Container(
+              width: 70,
+              height: 70,
+              margin: const EdgeInsets.only(right: 16),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(imageUrl),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(defaultRadius),
               ),
-              borderRadius: BorderRadius.circular(defaultRadius),
             ),
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  destination1,
-                  style: destinationStyle1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  destination2,
-                  style: destinationStyle2,
-                ),
-              ],
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    destination1,
+                    style: destinationStyle1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    destination2,
+                    style: destinationStyle2,
+                  ),
+                ],
+              ),
             ),
-          ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset(
-                  'assets/icon_star.png',
-                  width: 24,
-                ),
-                Text(
-                  rate.toString(),
-                  style: ratingStyle1,
-                )
-              ],
-            ),
-          )
-        ],
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    'assets/icon_star.png',
+                    width: 24,
+                  ),
+                  Text(
+                    rate.toString(),
+                    style: ratingStyle1,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
