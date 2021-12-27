3f5c54c9a985a9f3186aa1b91766e0ca06120e1c
aprxrt3 <ajiprxty3@gmail.com>
2021-12-27 17:09:23 +0700

    create seat availibility widget

--- lib/ui/widget/availability.dart
+++ lib/ui/widget/availability.dart
@@ -3,30 +3,71 @@ import 'package:flutter/material.dart';
 import '../../theme.dart';
 
 class AvailabilityWidget extends StatelessWidget {
-  
+  final int index;
 
-
-  const AvailabilityWidget({ Key? key }) : super(key: key);
+  const AvailabilityWidget({Key? key, required this.index}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-            margin: const EdgeInsets.only(bottom: 30),
-            width: 91,
-            height: 21,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset(
-                  'assets/icon_available.png',
-                  width: 16,
-                ),
-                Text(
-                  'Available',
-                  style: availableStyle,
-                ),
-              ],
+    backgroundColor() {
+      switch (index) {
+        case 0:
+          return availableColor;
+        case 1:
+          return kPrimaryColor;
+        case 2:
+          return unavailableColor;
+
+        default:
+          return unavailableColor;
+      }
+    }
+
+    borderColor() {
+      switch (index) {
+        case 0:
+          return kPrimaryColor;
+        case 1:
+          return kPrimaryColor;
+        case 2:
+          return unavailableColor;
+
+        default:
+          return unavailableColor;
+      }
+    }
+
+    child() {
+      switch (index) {
+        case 0:
+          return const SizedBox();
+        case 1:
+          return Center(
+            child: Text(
+              'YOU',
+              style: seatStyle2,
             ),
           );
+        case 2:
+          return const SizedBox();
+
+        default:
+          return const SizedBox();
+      }
+    }
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      width: 48,
+      height: 48,
+      decoration: BoxDecoration(
+        color: backgroundColor(),
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+          color: borderColor(),
+        ),
+      ),
+      child: child(),
+    );
   }
 }
