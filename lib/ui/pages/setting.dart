7c7aa6a60591430f5b0f2bc469b5390efa16d570
aprxrt3 <ajiprxty3@gmail.com>
2021-12-29 00:33:54 +0700

    add setting page for main page

---
+++ lib/ui/pages/setting.dart
@@ -0,0 +1,16 @@
+import 'package:airplane_ticketing/theme.dart';
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBgColor,
+      body: const Center(
+        child: Text('Walletting'),
+      ),
+    );
+  }
+}
