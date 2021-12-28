72b390b493e166b1b2e771cb9465df4f6a173f6c
aprxrt3 <ajiprxty3@gmail.com>
2021-12-28 23:46:07 +0700

    build main page for slider menu home, wallet, setting etc

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:airplane_ticketing/ui/pages/bonus.dart';
 import 'package:airplane_ticketing/ui/pages/checkout_page.dart';
 import 'package:airplane_ticketing/ui/pages/detail.dart';
 import 'package:airplane_ticketing/ui/pages/home.dart';
+import 'package:airplane_ticketing/ui/pages/main_page.dart';
 import 'package:airplane_ticketing/ui/pages/onboarding.dart';
 import 'package:airplane_ticketing/ui/pages/seat_page.dart';
 import 'package:airplane_ticketing/ui/pages/sign_up.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
         '/onboard': (context) => const OnboardPage(),
         '/sign_up': (context) => const SignUpPage(),
         '/bonus': (context) => const BonusPage(),
-        '/home': (context) => const HomePage(),
+        '/home': (context) => const MainPage(),
         '/detail': (context) => const DetailPage(),
         '/seat': (context) => const SeatPage(),
         '/checkout': (context) => const COPage(),
