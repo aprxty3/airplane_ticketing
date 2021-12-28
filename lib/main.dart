948a45831c6eaaa9decd8569674c6e8affae4dc7
aprxrt3 <ajiprxty3@gmail.com>
2021-12-29 01:20:46 +0700

    add bloc multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:airplane_ticketing/cubit/pages_cubit.dart';
 import 'package:airplane_ticketing/ui/pages/bonus.dart';
 import 'package:airplane_ticketing/ui/pages/checkout_page.dart';
 import 'package:airplane_ticketing/ui/pages/detail.dart';
-import 'package:airplane_ticketing/ui/pages/home.dart';
+
 import 'package:airplane_ticketing/ui/pages/main_page.dart';
 import 'package:airplane_ticketing/ui/pages/onboarding.dart';
 import 'package:airplane_ticketing/ui/pages/seat_page.dart';
@@ -9,6 +10,7 @@ import 'package:airplane_ticketing/ui/pages/sign_up.dart';
 import 'package:airplane_ticketing/ui/pages/splash.dart';
 import 'package:airplane_ticketing/ui/pages/success.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(const MyApp());
 
@@ -17,19 +19,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => const SplashPage(),
-        '/onboard': (context) => const OnboardPage(),
-        '/sign_up': (context) => const SignUpPage(),
-        '/bonus': (context) => const BonusPage(),
-        '/home': (context) => const MainPage(),
-        '/detail': (context) => const DetailPage(),
-        '/seat': (context) => const SeatPage(),
-        '/checkout': (context) => const COPage(),
-        '/success': (context) => const SuccessPage(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => PagesCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => const SplashPage(),
+          '/onboard': (context) => const OnboardPage(),
+          '/sign_up': (context) => const SignUpPage(),
+          '/bonus': (context) => const BonusPage(),
+          '/home': (context) => const MainPage(),
+          '/detail': (context) => const DetailPage(),
+          '/seat': (context) => const SeatPage(),
+          '/checkout': (context) => const COPage(),
+          '/success': (context) => const SuccessPage(),
+        },
+      ),
     );
   }
 }
