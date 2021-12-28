5235051a4fe592e5c19fbdc118a9023c99db5026
aprxrt3 <ajiprxty3@gmail.com>
2021-12-29 02:53:04 +0700

    implement firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,16 @@ import 'package:airplane_ticketing/ui/pages/seat_page.dart';
 import 'package:airplane_ticketing/ui/pages/sign_up.dart';
 import 'package:airplane_ticketing/ui/pages/splash.dart';
 import 'package:airplane_ticketing/ui/pages/success.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
