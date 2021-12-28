b3735f8714cacd7144dec698555682dce9b16036
aprxrt3 <ajiprxty3@gmail.com>
2021-12-28 23:47:06 +0700

    add transaction page

---
+++ lib/ui/pages/transaction.dart
@@ -0,0 +1,16 @@
+import 'package:airplane_ticketing/theme.dart';
+import 'package:flutter/material.dart';
+
+class TransactionPage extends StatelessWidget {
+  const TransactionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBgColor,
+      body: Column(
+        children: [Text('HAHAHAHAH')],
+      ),
+    );
+  }
+}
