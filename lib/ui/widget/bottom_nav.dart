c5ccd0ba95c7d9a093fa50a4546d5b900399566a
aprxrt3 <ajiprxty3@gmail.com>
2021-12-29 01:59:04 +0700

    add bloc state

--- lib/ui/widget/bottom_nav.dart
+++ lib/ui/widget/bottom_nav.dart
@@ -1,40 +1,48 @@
+import 'package:airplane_ticketing/cubit/pages_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../theme.dart';
 
 class BottomNavIcon extends StatelessWidget {
+  final int index;
   final String imageUrl;
-  final bool isActive;
 
   const BottomNavIcon({
     Key? key,
+    required this.index,
     required this.imageUrl,
-    this.isActive = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Container(
-          width: 24,
-          height: 24,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(imageUrl),
+    return GestureDetector(
+      onTap: () {
+        context.read<PagesCubit>().setPages(index);
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Image.asset(
+            imageUrl,
+            width: 24,
+            height: 24,
+            color: context.read<PagesCubit>().state == index
+                ? kPrimaryColor
+                : sGreyColor,
+          ),
+          Container(
+            width: 30,
+            height: 2,
+            decoration: BoxDecoration(
+              color: context.read<PagesCubit>().state == index
+                  ? kPrimaryColor
+                  : Colors.transparent,
+              borderRadius: BorderRadius.circular(defaultRadius),
             ),
-          ),
-        ),
-        Container(
-          width: 30,
-          height: 2,
-          decoration: BoxDecoration(
-            color: isActive ? kPrimaryColor : Colors.transparent,
-            borderRadius: BorderRadius.circular(defaultRadius),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
