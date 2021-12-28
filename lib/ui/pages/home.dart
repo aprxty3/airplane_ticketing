2d53977e029a3229e49d2b8b6540b2bbd20f6612
aprxrt3 <ajiprxty3@gmail.com>
2021-12-28 23:46:32 +0700

    clear bottom nav and move to mainpage

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -157,48 +157,11 @@ class HomePage extends StatelessWidget {
       );
     }
 
-    Widget bottomNav() {
-      return Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          // padding: const EdgeInsets.only(top: 18),
-          width: double.infinity,
-          height: 60,
-          margin: EdgeInsets.only(
-              bottom: 30, left: defaultMargin, right: defaultMargin),
-          decoration: BoxDecoration(
-            color: sWhiteColor,
-            borderRadius: BorderRadius.circular(defaultRadius),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const [
-              BottomNavIcon(
-                imageUrl: 'assets/icon_home.png',
-                isActive: true,
-              ),
-              BottomNavIcon(
-                  imageUrl: 'assets/icon_booking.png', isActive: false),
-              BottomNavIcon(
-                imageUrl: 'assets/icon_card.png',
-                isActive: false,
-              ),
-              BottomNavIcon(
-                imageUrl: 'assets/icon_settings.png',
-                isActive: false,
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-
     return Scaffold(
       backgroundColor: kBgColor,
       body: Stack(
         children: [
           mainPage(),
-          bottomNav(),
         ],
       ),
     );
