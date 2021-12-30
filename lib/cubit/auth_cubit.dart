3884cc2c06e7f9a34c3f19d908637063abfecd6d
aprxrt3 <ajiprxty3@gmail.com>
2021-12-30 14:26:21 +0700

    add auth cubit

---
+++ lib/cubit/auth_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:airplane_ticketing/model/user_model.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+}
