2c30d06c503f561c804b9450d8d96dda7110f3be
ajiprasetyo <ajiprasetyo@digitalsekuriti.id>
2022-01-08 14:34:34 +0700

    build auth service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,30 @@
+import 'package:airplane_ticketing/model/user_model.dart';
+import 'package:airplane_ticketing/services/user_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<UserModel> signUp(
+      {required String email,
+      required String password,
+      required String name,
+      String hobby = ''}) async {
+    try {
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      UserModel user = UserModel(
+        id: userCredential.user!.uid,
+        email: email,
+        name: name,
+        balance: 28000000,
+      );
+      await UserService().setUser(user);
+
+      return user;
+    } catch (e) {
+      throw e;
+    }
+  }
+}
