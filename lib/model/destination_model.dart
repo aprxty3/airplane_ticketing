a03b133445cbf1be5ac21d66991d56f299294ddd
aprxty3 <ajiprxty3@gmail.com>
2022-01-08 23:06:20 +0700

    add Equatable parameter

--- lib/model/destination_model.dart
+++ lib/model/destination_model.dart
@@ -1,13 +1,40 @@
-class destinationModel {
-  final String imageUrl;
-  final double rate;
-  final String destination1;
-  final String destination2;
+import 'package:equatable/equatable.dart';
 
-  destinationModel({
+class DestinationModel extends Equatable {
+  final String id;
+  final String name;
+  final String city;
+  final String imageUrl;
+  final double rating;
+  final int price;
+
+  DestinationModel({
+    required this.id,
+    this.name = '',
+    this.city = '',
     this.imageUrl = '',
-    this.rate = 0.0,
-    this.destination1 = '',
-    this.destination2 = '',
+    this.rating = 0.0,
+    this.price = 0,
   });
+
+  factory DestinationModel.fromJson(String id, Map<String, dynamic> json) =>
+      DestinationModel(
+        id: id,
+        name: json['name'],
+        city: json['city'],
+        imageUrl: json['imageUrl'],
+        rating: json['rating'],
+        price: json['price'],
+      );
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        id,
+        name,
+        city,
+        imageUrl,
+        rating,
+        price,
+      ];
 }
