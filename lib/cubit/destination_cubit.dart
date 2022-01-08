4940b64b91e2102b3da837ef21aca9315abd1b09
aprxty3 <ajiprxty3@gmail.com>
2022-01-08 23:05:53 +0700

    create Destination Cubit & State

---
+++ lib/cubit/destination_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:airplane_ticketing/model/destination_model.dart';
+import 'package:airplane_ticketing/services/destination_service.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'destination_state.dart';
+
+class DestinationCubit extends Cubit<DestinationState> {
+  DestinationCubit() : super(DestinationInitial());
+
+  void fetchDestionations() async {
+    try {
+      emit(DestinationLoading());
+
+      List<DestinationModel> destinations =
+          await DestinationService().fetchDestinations();
+
+      emit(DestinationSuccess(destinations));
+    } catch (e) {
+      emit(DestinationFailed(e.toString()));
+    }
+  }
+}
