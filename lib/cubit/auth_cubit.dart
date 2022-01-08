1384e92595968a7a21ab48b3815236db35b2c7ce
ajiprasetyo <ajiprasetyo@digitalsekuriti.id>
2022-01-08 14:57:45 +0700

    add signUp parameter

--- lib/cubit/auth_cubit.dart
+++ lib/cubit/auth_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:airplane_ticketing/model/user_model.dart';
+import 'package:airplane_ticketing/services/auth_service.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -6,4 +7,26 @@ part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
+
+  void signUp({
+    required String email,
+    required String password,
+    required String name,
+    String hobby = '',
+  }) async {
+    try {
+      emit(AuthLoading());
+
+      UserModel user = await AuthService().signUp(
+        email: email,
+        password: password,
+        name: name,
+        hobby: hobby,
+      );
+
+      emit(AuthSuccess(user));
+    } catch (e) {
+      emit(AuthFailed(e.toString()));
+    }
+  }
 }
