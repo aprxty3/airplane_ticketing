e1f13348714cc05fb297a178f4344f96e95c2f00
aprxty3 <ajiprxty3@gmail.com>
2022-01-08 16:42:15 +0700

    build log out page

--- lib/ui/pages/setting.dart
+++ lib/ui/pages/setting.dart
@@ -1,16 +1,48 @@
+import 'package:airplane_ticketing/cubit/auth_cubit.dart';
 import 'package:airplane_ticketing/theme.dart';
+import 'package:airplane_ticketing/ui/widget/button_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kBgColor,
-      body: const Center(
-        child: Text('Walletting'),
-      ),
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthFailed) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(state.error),
+              backgroundColor: Colors.red,
+            ),
+          );
+        } else if (state is AuthInitial) {
+          Navigator.pushNamedAndRemoveUntil(
+              context, '/sign_up', (route) => false);
+        }
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        if (state is AuthLoading) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return Scaffold(
+          backgroundColor: kBgColor,
+          body: Center(
+            child: ButtonWidget(
+              title: 'LOG OUT',
+              onPressed: () {
+                context.read<AuthCubit>().signOut();
+              },
+              width: 220,
+            ),
+          ),
+        );
+      },
     );
   }
 }
