5836c0313a01c24c7291ecfa36541bda51d78a62
aprxty3 <ajiprxty3@gmail.com>
2022-01-08 20:26:25 +0700

    create signIn parameter

--- lib/cubit/auth_cubit.dart
+++ lib/cubit/auth_cubit.dart
@@ -9,6 +9,22 @@ part 'auth_state.dart';
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
 
+  void signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      emit(AuthLoading());
+      UserModel user = await AuthService().signIn(
+        email: email,
+        password: password,
+      );
+      emit(AuthSuccess(user));
+    } catch (e) {
+      emit(AuthFailed(e.toString()));
+    }
+  }
+
   void signUp({
     required String email,
     required String password,
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -5,6 +5,25 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
+  Future<UserModel> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      UserModel user =
+          await UserService().getUserById(userCredential.user!.uid);
+
+      return user;
+    } catch (e) {
+      throw e;
+    }
+  }
+
   Future<UserModel> signUp(
       {required String email,
       required String password,
