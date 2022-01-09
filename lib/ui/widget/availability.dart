7b351f98a18491d8c9f0cc000d2138b56806bb88
aprxty3 <ajiprxty3@gmail.com>
2022-01-09 15:00:35 +0700

    add id parameter

--- lib/ui/widget/availability.dart
+++ lib/ui/widget/availability.dart
@@ -1,11 +1,17 @@
+import 'package:airplane_ticketing/cubit/seat_cubit.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../theme.dart';
 
 class AvailabilityWidget extends StatelessWidget {
   final int index;
+  final String id;
 
-  const AvailabilityWidget({Key? key, required this.index}) : super(key: key);
+  const AvailabilityWidget({
+    Key? key,
+    required this.index,
+    required this.id,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -56,19 +62,24 @@ class AvailabilityWidget extends StatelessWidget {
       }
     }
 
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      width: 48,
-      height: 48,
-      decoration: BoxDecoration(
-        color: backgroundColor(),
-        borderRadius: BorderRadius.circular(15),
-        border: Border.all(
-          color: borderColor(),
-          width: 2,
+    return GestureDetector(
+      onTap: () {
+        context.read<SeatCubit>().selectSeat(id);
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        width: 48,
+        height: 48,
+        decoration: BoxDecoration(
+          color: backgroundColor(),
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(
+            color: borderColor(),
+            width: 2,
+          ),
         ),
+        child: child(),
       ),
-      child: child(),
     );
   }
 }
