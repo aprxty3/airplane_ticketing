dd8cb022c88aeb54194c6d39f75d2eec708bf020
aprxty3 <ajiprxty3@gmail.com>
2022-01-09 23:04:00 +0700

    slicing design wallet

--- lib/ui/pages/wallet.dart
+++ lib/ui/pages/wallet.dart
@@ -1,16 +1,89 @@
+import 'package:airplane_ticketing/cubit/auth_cubit.dart';
 import 'package:airplane_ticketing/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class WalletPage extends StatelessWidget {
   const WalletPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kBgColor,
-      body: const Center(
-        child: Text('Walletting'),
-      ),
+    return BlocBuilder<AuthCubit, AuthState>(
+      builder: (context, state) {
+        if (state is AuthSuccess) {
+          return Center(
+            child: Container(
+              width: 300,
+              height: 211,
+              padding: EdgeInsets.all(defaultMargin),
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: AssetImage('assets/image_card.png'),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: kPrimaryColor.withOpacity(0.5),
+                    blurRadius: 50,
+                    offset: const Offset(0, 10),
+                  ),
+                ],
+              ),
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Name',
+                            style: bonusNameStyle1,
+                          ),
+                          Text(
+                            state.user.name,
+                            style: bonusNameStyle2,
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/icon_plane.png',
+                            width: 24,
+                          ),
+                          const SizedBox(
+                            width: 6,
+                          ),
+                          Text(
+                            'Pay',
+                            style: payStyle,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Text(
+                    'Balance',
+                    style: bonusNameStyle1,
+                  ),
+                  Text(
+                    'IDR 280.000.000',
+                    style: bonusNameStyle3,
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return const SizedBox();
+        }
+      },
     );
   }
 }
