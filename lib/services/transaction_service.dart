7a165d557966bcd0b01c57ef740657068912b0c2
aprxty3 <ajiprxty3@gmail.com>
2022-01-09 22:28:36 +0700

    add Future parameter

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -21,4 +21,20 @@ class TransactionService {
       throw e;
     }
   }
+  Future<List<TransactionModel>> fetchTransactions() async {
+    try {
+      QuerySnapshot result = await _transactionReference.get();
+
+      List<TransactionModel> transactions = result.docs.map(
+        (e) {
+          return TransactionModel.fromJson(
+              e.id, e.data() as Map<String, dynamic>);
+        },
+      ).toList();
+
+      return transactions;
+    } catch (e) {
+      throw e;
+    }
+  }
 }
