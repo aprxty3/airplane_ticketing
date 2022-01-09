967a33569aedc6cad3400f7ded913c3ba9d4c7a5
aprxty3 <ajiprxty3@gmail.com>
2022-01-09 22:27:57 +0700

    implement Success Parameter

--- lib/cubit/transaction_state.dart
+++ lib/cubit/transaction_state.dart
@@ -11,7 +11,14 @@ class TransactionInitial extends TransactionState {}
 
 class TransactionLoading extends TransactionState {}
 
-class TransactionSuccess extends TransactionState {}
+class TransactionSuccess extends TransactionState {
+  final List<TransactionModel> transactions;
+
+  TransactionSuccess(this.transactions);
+
+  @override
+  List<Object> get props => [transactions];
+}
 
 class TransactionFailed extends TransactionState {
   final String error;
