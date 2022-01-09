cad12b1bdb6c965fd517b2bc9f0a418304a4d655
aprxty3 <ajiprxty3@gmail.com>
2022-01-09 20:21:48 +0700

    add state for selected seat

--- lib/cubit/seat_cubit.dart
+++ lib/cubit/seat_cubit.dart
@@ -6,8 +6,22 @@ class SeatCubit extends Cubit<List<String>> {
 
   void selectSeat(String id) {
     print('prev state: $state');
-    state.add(id);
+    if (!isSelected(id)) {
+      state.add(id);
+    } else {
+      state.remove(id);
+    }
     print('new state: $state');
-    emit(state);
+    emit(List.from(state));
+  }
+
+  bool isSelected(String id) {
+    int index = state.indexOf(id);
+    print('index $index');
+    if (index == -1) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
