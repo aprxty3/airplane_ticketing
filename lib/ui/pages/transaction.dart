a809ef0a3483e727481e82bac1505e75997aa9b2
aprxty3 <ajiprxty3@gmail.com>
2022-01-09 22:57:18 +0700

    fetchData

--- lib/ui/pages/transaction.dart
+++ lib/ui/pages/transaction.dart
@@ -1,16 +1,54 @@
+import 'package:airplane_ticketing/cubit/transaction_cubit.dart';
 import 'package:airplane_ticketing/theme.dart';
+import 'package:airplane_ticketing/ui/widget/transaction_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class TransactionPage extends StatelessWidget {
+class TransactionPage extends StatefulWidget {
   const TransactionPage({Key? key}) : super(key: key);
 
   @override
+  _TransactionPageState createState() => _TransactionPageState();
+}
+
+class _TransactionPageState extends State<TransactionPage> {
+  @override
+  void initState() {
+    context.read<TransactionCubit>().fetchTransactions();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kBgColor,
-      body: const Center(
-        child: Text('Transaction'),
-      ),
+    return BlocBuilder<TransactionCubit, TransactionState>(
+      builder: (context, state) {
+        if (state is TransactionLoading) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state is TransactionSuccess) {
+          if (state.transactions.length == 0) {
+            return Center(
+              child: Text('Kamu belum memiliki transaksi'),
+            );
+          } else {
+            return ListView.builder(
+                itemCount: state.transactions.length,
+                padding: EdgeInsets.symmetric(
+                  horizontal: defaultMargin,
+                ),
+                itemBuilder: (context, index) {
+                  return TransactionCard(
+                    state.transactions[index],
+                  );
+                });
+          }
+        }
+
+        return Center(
+          child: Text('Transaction Page'),
+        );
+      },
     );
   }
 }
