2dbc28888cf02417b36b5b79e762c4f547b8290c
aprxty3 <ajiprxty3@gmail.com>
2022-01-09 21:06:58 +0700

    build Transaction Service

---
+++ lib/services/transaction_service.dart
@@ -0,0 +1,24 @@
+import 'package:airplane_ticketing/model/transaction_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class TransactionService {
+  final CollectionReference _transactionReference =
+      FirebaseFirestore.instance.collection('transactions');
+
+  Future<void> createTransaction(TransactionModel transaction) async {
+    try {
+      _transactionReference.add({
+        'destinations': transaction.destination.toJson(),
+        'amountOfTraveler': transaction.amountOfTraveler,
+        'selectedSeats': transaction.selectedSeats,
+        'insurance': transaction.insurance,
+        'refundable': transaction.refundable,
+        'vat': transaction.vat,
+        'price': transaction.price,
+        'grandTotal': transaction.grandTotal,
+      });
+    } catch (e) {
+      throw e;
+    }
+  }
+}
